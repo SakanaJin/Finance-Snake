932ae6b4bc0fd639d42da6c4188908cbac7075b6
Devin Ford <Devin.Ford4263@gmail.com>
2024-09-23 18:44:43 -0500

    pie chart labels

--- src/main.py
+++ src/main.py
@@ -16,17 +16,22 @@ def main() -> None:
     ctk.set_default_color_theme('blue')
     # ctk.set_default_color_theme('dark-blue')
     root = ctk.CTk()
-    root.geometry("500x350")
+    root.geometry("500x475")
     root.title('Finance Snake')
     root.wm_iconbitmap("./favicon.ico")
     root.grid_columnconfigure(0, weight=1)
     button = ctk.CTkButton(root, text='Test', command=button_action)
     button.grid(row=0, column=0, padx=20, pady=20, sticky='ew')
 
+    #need to have seperate method for updating pie chart
     y = np.array([35,25,25,15])
     fig1, ax = plt.subplots()
     fig1.set_facecolor(BACKGROUND_COLOR_DARK)
-    ax.pie(y)
+    patches, texts, autotexts = ax.pie(y, labels=['your mom', 'your dad', 'your bald grandma', 'your wrinkly grandpa'], autopct="%1.1f%%")
+    for text in texts:
+        text.set_color('white')
+    for autotext in autotexts:
+        autotext.set_color('white')
 
     canvas = FigureCanvasTkAgg(figure=fig1, master=root)
     widget = canvas.get_tk_widget()
